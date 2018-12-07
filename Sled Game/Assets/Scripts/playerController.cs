4199a7e6d08fc079660ba9c376d1e5aed51e880f
Aaron <aaron@mvcode.com>
2018-12-07 13:27:47 -0800

    Adding Game
    My first commit will be adding the Unity file

---
+++ Sled Game/Assets/Scripts/playerController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour {
+
+    Vector3 offset;
+    public GameObject snowParticles;
+    public Vector3 velocity;
+    Rigidbody rb;
+    bool start;
+
+	// Use this for initialization
+	void Start () {
+        offset = Camera.main.transform.position - this.transform.position;
+        rb = this.GetComponent<Rigidbody>();
+        start = true;
+        velocity = Vector3.zero;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Camera.main.transform.position = this.transform.position + offset;
+        if (!start)
+        {
+            velocity = rb.velocity;
+            Quaternion angle = Quaternion.LookRotation(velocity, Vector3.up * 90f);
+           
+            this.transform.rotation = angle;
+        }   
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        start = false;
+        
+        Quaternion angle = Quaternion.LookRotation(velocity, Vector3.up * 90f);
+        if (transform.rotation.eulerAngles.x - angle.x > 70)
+        {
+            Debug.Log("Crash");
+            Instantiate(snowParticles, this.transform.position, this.transform.rotation);
+            Destroy(this.gameObject);
+        }
+    }
+
+}
